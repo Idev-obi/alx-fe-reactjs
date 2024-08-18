90d0327d7987489d879a5949f60ee765413e36c3
Idev-obi <obinneopara@gmail.com>
2024-08-18 19:23:51 +0100

    hello

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// Footer.jsx
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{border: '2px solid white', backgroundColor: '#000'}}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+// MainContent.jsx
+import React from 'react';
+
+const MainContent = () => {
+  return (
+    <main style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+      <p style={{ fontWeight: 'bold' }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+};
+
+export default MainContent;
