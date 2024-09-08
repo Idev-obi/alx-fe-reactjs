6f618cf35d43168c44c4c631c71d29d8292d216b
Idev-obi <obinneopara@gmail.com>
2024-09-08 15:43:58 +0100

    hello

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -5,13 +5,20 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}", // Ensure to include all relevant paths
     "./public/index.html",        // This includes the public HTML file
   ],
-  darkMode: false, // Or 'media' or 'class' for dark mode support
+  darkMode: media, // Or 'media' or 'class' for dark mode support
   theme: {
-    extend: {}, // Add any custom theme extensions here
+    extend: {
+      colors:{
+        customBlue: '#1E40Af'
+      },
+    }, // Add any custom theme extensions here
   },
   variants: {
     extend: {}, // Extend variants for different utilities
   },
-  plugins: [], // Add any Tailwind plugins here
+  plugins: [ 
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ], // Add any Tailwind plugins here
 }
 
