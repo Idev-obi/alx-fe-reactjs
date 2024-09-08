1e1814602ead03a3515e4fb0d82bf4c4d4659ef8
Idev-obi <obinneopara@gmail.com>
2024-09-08 15:48:36 +0100

    hello

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,8 +1,11 @@
 // src/App.js
+import React from 'react';
+import UserProfile from './components/UserProfile';
+
 function App() {
   return (
-    <div className="bg-blue-500 text-white p-4">
-      <h1 className="text-2xl font-bold">Hello, Tailwind CSS!</h1>
+    <div className="App">
+      <UserProfile />
     </div>
   );
 }
