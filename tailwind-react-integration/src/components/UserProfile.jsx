65f08a8f44cf7db72da5d1a10d3debf0a4e2f79a
Idev-obi <obinneopara@gmail.com>
2024-09-15 20:01:16 +0100

    holla

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,11 @@
+
 import React from 'react';
 
 const UserProfile = () => {
   return (
     <div className="container mx-auto p-4 sm:p-6 md:p-8 lg:p-12 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg">
       <div className="flex flex-col items-center">
-        <img 
+          <img 
           src="profile.jpg" 
           alt="Profile Picture" 
           className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 lg:w-40 lg:h-40 rounded-full"
@@ -21,3 +22,4 @@ const UserProfile = () => {
 };
 
 export default UserProfile;
+
